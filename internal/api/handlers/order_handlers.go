13f4036066a899130f65b9a74662f82f2bfd9d2a
nabibek <nabibiek@mail.ru>
2025-05-24 04:47:47 +0500

    ADD: order_handler

--- internal/api/handlers/order_handlers.go
+++ internal/api/handlers/order_handlers.go
@@ -33,3 +33,63 @@ func (h *OrderHandler) CreateOrder(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(order)
 }
+
+func (h *OrderHandler) Orders(w http.ResponseWriter, r *http.Request) {
+	orders, err := h.orderServise.Orders(r.Context())
+	if err != nil {
+		http.Error(w, "failed to get orders", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(orders)
+}
+
+func (h *OrderHandler) UpdateOrdeItem(w http.ResponseWriter, r *http.Request) {
+	var input models.OrderItems
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+	err := h.orderServise.UpdateOrdeItemrByID(r.Context(), input)
+	if err != nil {
+		http.Error(w, "failed to update order Item: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message":"Order item updated successfully"}`))
+}
+
+func (h *OrderHandler) DeleteOrderByID(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id") // FIX
+	if idStr == "" {
+		http.Error(w, "missing id", http.StatusBadRequest)
+		return
+	}
+	err := h.orderServise.DeleteOrderByID(r.Context(), idStr)
+	if err != nil {
+		http.Error(w, "failed to delete order: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message":"Order deleted successfully"}`))
+}
+
+func (h *OrderHandler) GetOrderByID(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id") // FIX
+	if idStr == "" {
+		http.Error(w, "missing id", http.StatusBadRequest)
+		return
+	}
+	order, err := h.orderServise.GetOrderByID(r.Context(), idStr)
+	if err != nil {
+		http.Error(w, "order not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(order)
+}
