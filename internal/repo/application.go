fa476705cafaccaba66f9ac0a1c9cb14c0c2545a
Nabibek <nabibiek@mail.ru>
2025-05-28 11:39:53 +0300

    add Search application

--- internal/repo/application.go
+++ internal/repo/application.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"frappuccino/models"
 )
 
 type ApplicationRepo interface {
-	TotalPrice(ctx context.Context) (float64, error)
+	TotalPrice() (float64, error)
 	Search(ctx context.Context)
 }
 
@@ -15,14 +16,14 @@ type ApplicationRepository struct {
 	db *sql.DB
 }
 
-func NewApplicationRepository(db *sql.DB) *ApplicationRepo {
-	return &ApplicationRepo{db: db}
+func NewApplicationRepository(db *sql.DB) *ApplicationRepository {
+	return &ApplicationRepository{db: db}
 }
 
-func (r *ApplicationRepository) TotalPrice(ctx context.Context) (float64, error) {
+func (r *ApplicationRepository) TotalPrice() (float64, error) {
 	var totalPrice float64
 
-	err := r.db.QueryRowContext(ctx, `
+	err := r.db.QueryRow(`
 		SELECT SUM(total_price)
 		FROM orders
 		WHERE order_status = 'COMPLETED'
@@ -36,4 +37,39 @@ func (r *ApplicationRepository) TotalPrice(ctx context.Context) (float64, error)
 	}
 
 	return totalPrice, nil
+}
+func (r *ApplicationRepository) Search(ctx context.Context, q string, filters []string, minPrice, maxPrice *float64) (SearchResult, error) {
+	for _, filter := range filters {
+		if filter == "menu" {
+			searchMenu, err := SearchMenu(ctx, q, filter, minPrice, maxPrice)
+			if err != nil {
+				return nil, err
+			}
+		} else if filter == "order" {
+			searchOrder, err := SearchOrder(ctx, q, filter, minPrice, maxPrice)
+			if err != nil {
+				return nil, err
+			}
+		} else if filter == "0" || filter == "all" {
+			searchMenu, err := SearchMenu(ctx, q, filter, minPrice, maxPrice)
+			if err != nil {
+				return nil, err
+			}
+			searchOrder, err := SearchOrder(ctx, q, filter, minPrice, maxPrice)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+	}
+	return models.Search{
+		MenuItems:  []searchMenu,
+		OrderItems: []searchOrder,
+	}
+}
+func (r *ApplicationRepository) searchOrder(ctx context.Context, q string, filters []string, minPrice, maxPrice *float64) ([]models.SearchOrder, error) {
+	var allOrders []models.SearchOrder
+	query := `
+SELECT o.order_id,c.customer_name,oi.item_name, 		
+`
 }
